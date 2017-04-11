6eb9e1c037f6852bfd21ddadbc4a4f4a6edca235
CurtisJoy <curts_joy@hotmail.com>
2017-04-11 13:40:33 -0400

    Update UserT.java
    Adding list of book ratings

--- UserT.java
+++ UserT.java
@@ -4,14 +4,19 @@ public class UserT {
 
 	private int User;
 	private ArrayList<String> Books;
+	private ArrayList<Integer> Ratings;
 	
 	public UserT(int user){
 		this.User = user;
-		this.Books = new ArrayList<String>(0);		
+		this.Books = new ArrayList<String>(0);
+		this.Ratings = new ArrayList<Integer>(0);
 	}
 	
 	public int getUser(){ return this.User; }
 	public ArrayList<String> getBooks(){ return this.Books; }
-	public void addBook(String book){ this.Books.add(book); }
+	public void addBook(String book, int rating){ 
+		this.Books.add(book); 
+		this.Ratings.add(rating);
+	}
 	
 }
