c1b9554af7749713cd91a31746cd4910604f2210
CurtisJoy <curts_joy@hotmail.com>
2017-04-11 13:32:22 -0400

    Update BookT.java

--- BookT.java
+++ BookT.java
@@ -1,22 +1,25 @@
+import java.util.ArrayList;
+
 public class BookT {
 
 	private String title;
 	private String author;
 	private String ISBN;
-	private int user;
+	private ArrayList<Integer> Users;
 	
 	// ISBN, Title, Author from BX-Books.csv
 	// user from BX-Users.csv
-	public BookT(String bookTitle, String bookAuthor, String bookISBN, int user){
+	public BookT(String bookTitle, String bookAuthor, String bookISBN){
 		this.title = bookTitle;
 		this.author = bookAuthor;
 		this.ISBN = bookISBN;
-		this.user = user;
+		this.Users = new ArrayList<Integer>(0);
 	}
 	
 	public String getTitle(){ return this.title; }
 	public String getAuthor(){ return this.author; }
 	public String getID(){ return this.ISBN; }
-	public int getUser(){ return this.user; }
+	public ArrayList<Integer> getUsers(){ return this.Users; }
+	public void addUser(int user){ this.Users.add(user); }
 	
 }
